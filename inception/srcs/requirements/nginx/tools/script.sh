14d63cb8a319fa9644441908a83777050f35222f
candriam <candriam@inception.42antananarivo.mg>
2025-07-05 17:11:23 +0300

    update some files

--- inception/srcs/requirements/nginx/tools/script.sh
+++ inception/srcs/requirements/nginx/tools/script.sh
@@ -5,7 +5,7 @@ mkdir -p /etc/nginx/certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout /etc/nginx/certs/privkey.pem \
   -out /etc/nginx/certs/fullchain.pem \
-  -subj "/C=MO/L=KH/O=42/OU=student/CN=${DOAMIN_NAME}"
+  -subj "/C=MO/L=KH/O=42/OU=student/CN=${DOMAIN_NAME}"
 
 
 sed -i "s/\${DOMAIN_NAME}/$DOMAIN_NAME/g" /etc/nginx/conf.d/default.conf
